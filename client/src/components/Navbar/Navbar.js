f1650e1d3e6aaea177d9d980920ac4364209f2c3
Max Wo <maxwo@protonmail.com>
2019-05-19 02:04:04 +1000

    Finished navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,21 +1,35 @@
 /* React */
-import React from 'react';
+import React from "react";
+
+/* React Router */
+import { Link } from "react-router-dom";
 
 /* Ant Design components */
-import {Layout, Typography} from "antd";
+import { Col, Icon, Layout, Row } from "antd";
 
-/* Styles */
-import styles from "./Navbar.module.css";
+/* Logo */
+import logo from "../../assets/logo.png";
 
-const {Header} = Layout;
-const {Title} = Typography;
+const { Header } = Layout;
 
 const Navbar = () => (
   <Layout>
-    <Header className={styles.header}>
-      <Typography>
-        <Title>Examplar</Title>
-      </Typography>
+    <Header id="navbar">
+      <Row type="flex" align="middle">
+
+        <Col span={8}>
+          <Link to="/">
+            <img id="logo" src={logo} alt="logo"/>
+          </Link>
+        </Col>
+
+        <Col span={8} offset={8}>
+          <Icon className="icon" type="github" onClick={() => {
+            window.open("https://github.com/maxwowo/examplar", "_blank");
+          }}/>
+        </Col>
+
+      </Row>
     </Header>
   </Layout>
 );
