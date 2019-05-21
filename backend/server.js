0b20df447790dbd2b3b1babca5c69093901b3844
Max <maxwo@protonmail.com>
2019-05-22 02:16:14 +1200

    Established connection between search page and backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const mysql = require("mysql2");
 
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "examplardb"
+});
 
 // // this is our create methid
 // // this method adds new data in our database
@@ -31,8 +39,38 @@ app.use(logger("dev"));
 //   });
 // });
 
+router.get("/courses", function (req, res) {
+  const { query } = req;
+
+  res.send([
+    {
+      courseCode: "COMP1511",
+      courseName: "Fundamentals of Computing"
+    },
+    {
+      courseCode: "COMP2521",
+      courseName: "Data Structures and Algorithms"
+    },
+    {
+      courseCode: "COMP2111",
+      courseName: "System Modelling and Design"
+    },
+    {
+      courseCode: "MATH1131",
+      courseName: "Mathematics 1A"
+    },
+    {
+      courseCode: "MATH1231",
+      courseName: "Mathematics 1B"
+    }
+  ]);
+});
+
+
 // append /api for our http requests
 app.use("/api", router);
 
 // launch our backend into a port
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+app.listen(API_PORT, () => {
+  console.log(`LISTENING ON PORT ${API_PORT}`);
+});
--- client/src/components/SearchPage/SearchPage.js
+++ client/src/components/SearchPage/SearchPage.js
@@ -8,12 +8,25 @@ import Axios from "axios";
 import { withRouter } from "react-router-dom";
 
 class SearchPage extends Component {
-
+  state = {};
 
   componentDidMount() {
     const { location } = this.props;
 
     const params = new URLSearchParams(location.search);
+    const course = params.get("course");
+    const university = params.get("university");
+
+    Axios.get("/api/courses", {
+      params: {
+        course: !course ? "" : course,
+        university: !university ? "" : university
+      }
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      // console.log(err);
+    });
   }
 
   render() {
