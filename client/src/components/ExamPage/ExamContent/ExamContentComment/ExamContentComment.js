8158500253146147cf1e1d55af27c5b9594b1255
Max <maxwo@protonmail.com>
2019-05-30 03:27:05 +1200

    Added preview switch

--- client/src/components/ExamPage/ExamContent/ExamContentComment/ExamContentComment.js
+++ client/src/components/ExamPage/ExamContent/ExamContentComment/ExamContentComment.js
@@ -5,40 +5,53 @@ import React from "react";
 import { connect } from "react-redux";
 
 /* Ant Design components */
-import { Typography, Row, Col, Input, Button } from "antd";
+import { Typography, Row, Col, Input, Button, Switch } from "antd";
 
 /* Styles */
 import "./ExamContentComment.less";
 
 const { TextArea } = Input;
+const { Text } = Typography;
 
 const ExamContentComment = props => (
-  <Row
-    id="exam-content-comment-container"
-    type="flex"
-    align="middle"
-  >
-    <Col span={24}>
-      <TextArea
-        autosize={{minRows: 8}}
-        id="exam-content-comment-textarea"
-      />
-    </Col>
-    <Col span={24}>
-      <Button
-        type="primary"
-        id="exam-content-comment-comment-btn"
-      >
-        Comment
-      </Button>
+  <div>
+    <Row
+      id="exam-content-comment-container"
+      type="flex"
+      align="middle"
+    >
+      <Col span={24}>
+        <TextArea
+          autosize={{ minRows: 8 }}
+          id="exam-content-comment-textarea"
+        />
+      </Col>
+    </Row>
 
-      <Button
-        type="secondary"
-      >
-        Preview
-      </Button>
-    </Col>
-  </Row>
+    <Row
+      type="flex"
+      justify="space-between"
+      align="middle"
+    >
+      <Col>
+        <Button
+          type="primary"
+        >
+          Comment
+        </Button>
+      </Col>
+
+      <Col>
+        <Text
+          id="exam-content-comment-preview"
+        >
+          Preview
+        </Text>
+        <Switch/>
+      </Col>
+    </Row>
+  </div>
+
 );
 
 export default ExamContentComment;
