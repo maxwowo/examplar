34631f037942a011c75df10923520676159b12a9
Max <maxwo@protonmail.com>
2019-05-30 03:57:51 +1200

    Added button to menu

--- client/src/components/ExamPage/ExamSider/ExamSider.js
+++ client/src/components/ExamPage/ExamSider/ExamSider.js
@@ -5,7 +5,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 /* Ant Design components */
-import { Layout, Typography, Menu, Icon } from "antd";
+import { Button, Menu, Icon } from "antd";
 
 /* Styles */
 import "./ExamSider.less";
@@ -19,38 +19,49 @@ const mapStateToProps = state => (
 const ExamSider = (
   {}
 ) => (
-  <Menu
-    mode="inline"
-    id="exam-page-sider-menu"
+  <div
+    id="exam-page-sider"
   >
-    <SubMenu
-      key="sub1"
-      title={<span>Question One</span>}
+    <Menu
+      mode="inline"
+      id="exam-page-sider-menu"
     >
-      <Menu.Item key="1">1i)</Menu.Item>
-      <Menu.Item key="2">2i)</Menu.Item>
-      <Menu.Item key="3">2ii)</Menu.Item>
-      <Menu.Item key="4">2iii)</Menu.Item>
-    </SubMenu>
-    <SubMenu
-      key="sub2"
-      title={<span>Question Two</span>}
+      <SubMenu
+        key="sub1"
+        title={<span>Question One</span>}
+      >
+        <Menu.Item key="1">1i)</Menu.Item>
+        <Menu.Item key="2">2i)</Menu.Item>
+        <Menu.Item key="3">2ii)</Menu.Item>
+        <Menu.Item key="4">2iii)</Menu.Item>
+      </SubMenu>
+      <SubMenu
+        key="sub2"
+        title={<span>Question Two</span>}
+      >
+        <Menu.Item key="5">1i)</Menu.Item>
+        <Menu.Item key="6">2i)</Menu.Item>
+        <Menu.Item key="7">2ii)</Menu.Item>
+        <Menu.Item key="8">2iii)</Menu.Item>
+      </SubMenu>
+      <SubMenu
+        key="sub3"
+        title={<span>Question Three</span>}
+      >
+        <Menu.Item key="9">1i)</Menu.Item>
+        <Menu.Item key="10">2i)</Menu.Item>
+        <Menu.Item key="11">2ii)</Menu.Item>
+        <Menu.Item key="12">2iii)</Menu.Item>
+      </SubMenu>
+    </Menu>
+    <Button
+      type="primary"
+      shape="round"
+      id="exam-page-sider-add-big-question-btn"
     >
-      <Menu.Item key="5">1i)</Menu.Item>
-      <Menu.Item key="6">2i)</Menu.Item>
-      <Menu.Item key="7">2ii)</Menu.Item>
-      <Menu.Item key="8">2iii)</Menu.Item>
-    </SubMenu>
-    <SubMenu
-      key="sub3"
-      title={<span>Question Three</span>}
-    >
-      <Menu.Item key="9">1i)</Menu.Item>
-      <Menu.Item key="10">2i)</Menu.Item>
-      <Menu.Item key="11">2ii)</Menu.Item>
-      <Menu.Item key="12">2iii)</Menu.Item>
-    </SubMenu>
-  </Menu>
+      <Icon type="plus"/>
+    </Button>
+  </div>
 );
 
 export default connect(mapStateToProps)(ExamSider);
