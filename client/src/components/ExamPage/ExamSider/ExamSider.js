19c069faa6edef4bce599380fdffd2794a915d2f
Max <maxwo@protonmail.com>
2019-05-30 13:40:13 +1200

    Minor refactoring

--- client/src/components/ExamPage/ExamSider/ExamSider.js
+++ client/src/components/ExamPage/ExamSider/ExamSider.js
@@ -23,44 +23,40 @@ const mapStateToProps = state => (
 const ExamSider = (
   {}
 ) => (
-  <div
-    id="exam-page-sider"
+  <Menu
+    mode="inline"
+    id="exam-page-sider-menu"
   >
-    <Menu
-      mode="inline"
-      id="exam-page-sider-menu"
+    <SubMenu
+      key="sub1"
+      title={<span>Question One</span>}
     >
-      <SubMenu
-        key="sub1"
-        title={<span>Question One</span>}
-      >
-        <Menu.Item key="1">1i)</Menu.Item>
-        <Menu.Item key="2">2i)</Menu.Item>
-        <Menu.Item key="3">2ii)</Menu.Item>
-        <Menu.Item key="4">2iii)</Menu.Item>
-        <SubMenuButton/>
-      </SubMenu>
-      <SubMenu
-        key="sub2"
-        title={<span>Question Two</span>}
-      >
-        <Menu.Item key="5">1i)</Menu.Item>
-        <Menu.Item key="6">2i)</Menu.Item>
-        <Menu.Item key="7">2ii)</Menu.Item>
-        <Menu.Item key="8">2iii)</Menu.Item>
-      </SubMenu>
-      <SubMenu
-        key="sub3"
-        title={<span>Question Three</span>}
-      >
-        <Menu.Item key="9">1i)</Menu.Item>
-        <Menu.Item key="10">2i)</Menu.Item>
-        <Menu.Item key="11">2ii)</Menu.Item>
-        <Menu.Item key="12">2iii)</Menu.Item>
-      </SubMenu>
-    </Menu>
+      <Menu.Item key="1">1i)</Menu.Item>
+      <Menu.Item key="2">2i)</Menu.Item>
+      <Menu.Item key="3">2ii)</Menu.Item>
+      <Menu.Item key="4">2iii)</Menu.Item>
+      <SubMenuButton/>
+    </SubMenu>
+    <SubMenu
+      key="sub2"
+      title={<span>Question Two</span>}
+    >
+      <Menu.Item key="5">1i)</Menu.Item>
+      <Menu.Item key="6">2i)</Menu.Item>
+      <Menu.Item key="7">2ii)</Menu.Item>
+      <Menu.Item key="8">2iii)</Menu.Item>
+    </SubMenu>
+    <SubMenu
+      key="sub3"
+      title={<span>Question Three</span>}
+    >
+      <Menu.Item key="9">1i)</Menu.Item>
+      <Menu.Item key="10">2i)</Menu.Item>
+      <Menu.Item key="11">2ii)</Menu.Item>
+      <Menu.Item key="12">2iii)</Menu.Item>
+    </SubMenu>
     <MenuButton/>
-  </div>
+  </Menu>
 );
 
 export default connect(mapStateToProps)(ExamSider);
