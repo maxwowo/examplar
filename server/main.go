607555418c32a63021a2fb9cb0459b590f5d7dc1
Max Wo <maxwo@protonmail.com>
2019-12-29 00:48:26 +1100

    Directory structure

--- server/main.go
+++ server/main.go
@@ -16,15 +16,22 @@ func main() {
 		dbname   = "examplar"
 	)
 
-	connStr := fmt.Sprintf("user=%s password = %s dbname=%s host=%s port=%d sslmode=verify-full", user, password, dbname, host, port)
+	connStr := fmt.Sprintf("user=%s password = %s dbname=%s host=%s port=%d sslmode=disable", user, password, dbname, host, port)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT name FROM users WHERE age = 12")
+	rows, err := db.Query("SELECT * from public.university")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(rows)
+
+	var id int
+	var name string
+	rows.Next()
+	if err := rows.Scan(&id, &name); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(id, name)
 }
