81cc2ee5e3b96c029b56dc1efdb5fb0b0a34acd1
Max Wo <maxwo@protonmail.com>
2019-12-28 19:45:53 +1100

    Refactoring

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/maxwowo/examplar
+
+go 1.13
+
+require github.com/lib/pq v1.3.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "database/sql"
-    _ "gopkg.in/go-on/pq.v2"
-
-//     redis "gopkg.in/redis.v4"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
 )
 
 func main() {
-//     client := redis.NewClient(&redis.Options{
-//         Addr:     "redis:6379",
-//         Password: "", // no password set
-//         DB:       0,  // use default DB
-//     })
+	const (
+		host     = "localhost"
+		port     = 5432
+		user     = "examplar"
+		password = "examplar"
+		dbname   = "examplar"
+	)
 
+	connStr := fmt.Sprintf("user=%s password = %s dbname=%s host=%s port=%d sslmode=verify-full", user, password, dbname, host, port)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    const (
-        host = "localhost"
-        port = 5432
-        user = "examplar"
-        password = "password"
-        dbname = "examplar_db"
-    )
-
-    fmt.Println("hello world")
+	rows, err := db.Query("SELECT name FROM users WHERE age = 12")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(rows)
 }
