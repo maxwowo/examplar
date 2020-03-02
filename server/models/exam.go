cd11243ef88c77205a72fb1490e1b6c82fda34f9
Max Wo <maxwo@protonmail.com>
2020-03-03 02:26:49 +1100

    Add create exam model

--- server/models/exam.go
+++ server/models/exam.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/maxwowo/examplar/database"
+import (
+	"github.com/maxwowo/examplar/database"
+	"github.com/maxwowo/examplar/forms"
+)
 
 type Exam struct {
 	ID       int `json:"id"`
@@ -25,6 +28,28 @@ func (e Exam) Get(ID int) (*Exam, error) {
 	var exam Exam
 
 	err = stmt.QueryRow(ID).Scan(&exam.ID, &exam.ExamYear, &exam.ExamTerm, &exam.CourseID)
+
+	return &exam, err
+}
+
+func (e Exam) Create(examPayload forms.CreateExam) (*Exam, error) {
+	db := database.GetDatabase()
+
+	stmt, err := db.Prepare(`
+		INSERT INTO exams
+		(id, exam_year, exam_term, course_id)
+		VALUES
+		(DEFAULT, $1, $2, $3)
+		RETURNING id, exam_year, exam_term, course_id
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var exam Exam
+
+	err = stmt.QueryRow(examPayload.ExamYear, examPayload.ExamTerm, examPayload.CourseID).Scan(&exam.ID, &exam.ExamYear, &exam.ExamTerm, &exam.CourseID)
 
 	return &exam, err
 }
