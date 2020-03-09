9a305741ddb7e53ab1ceffc32ea71d5a728e0e47
Max Wo <maxwo@protonmail.com>
2020-03-09 22:10:25 +1100

    Create solution model

--- server/models/solution.go
+++ server/models/solution.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/maxwowo/examplar/database"
+import (
+	"github.com/maxwowo/examplar/database"
+	"github.com/maxwowo/examplar/forms"
+)
 
 type Solution struct {
 	ID      int    `json:"id"`
@@ -24,6 +27,28 @@ func (s Solution) Get(ID int) (*Solution, error) {
 	var solution Solution
 
 	err = stmt.QueryRow(ID).Scan(&solution.ID, &solution.Content, &solution.ExamID)
+
+	return &solution, err
+}
+
+func (s Solution) Create(solutionPaylaod forms.CreateSolution) (*Solution, error) {
+	db := database.GetDatabase()
+
+	stmt, err := db.Prepare(`
+		INSERT INTO solutions
+		(id, content, exam_id)
+		VALUES
+		(DEFAULT, $1, $2)
+		RETURNING id, content, exam_id
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var solution Solution
+
+	err = stmt.QueryRow(solutionPaylaod.Content, solutionPaylaod.ExamID).Scan(&solution.ID, &solution.Content, &solution.ExamID)
 
 	return &solution, err
 }
