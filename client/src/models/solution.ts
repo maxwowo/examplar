2868d714c074c167f1c8f9df15f72fff01aa7330
Max Wo <maxwo@protonmail.com>
2020-03-09 22:28:29 +1100

    Create solution model

---
+++ client/src/models/solution.ts
@@ -0,0 +1,69 @@
+import client, { ClientError, ClientResponse } from '../services/networking';
+
+export interface Solution {
+  id: number;
+  content: string;
+  examId: number;
+}
+
+interface GetBody {
+  solution: Solution;
+}
+
+interface CreateBody {
+  solution: Solution;
+}
+
+const solutionModel = {
+  get: (
+    solutionId: number
+  ): Promise<GetBody> => (
+    client
+      .get(
+        `/solutions/${solutionId}`
+      )
+      .then(
+        (
+          res: ClientResponse<GetBody>
+        ) => (
+          res.data
+        )
+      )
+      .catch(
+        (
+          err: ClientError
+        ) => {
+          throw err;
+        }
+      )
+  ),
+  create: (
+    content: number,
+    examId: number
+  ): Promise<CreateBody> => (
+    client
+      .post(
+        '/solutions',
+        {
+          content: content,
+          examId: examId
+        }
+      )
+      .then(
+        (
+          res: ClientResponse<CreateBody>
+        ) => (
+          res.data
+        )
+      )
+      .catch(
+        (
+          err: ClientError
+        ) => {
+          throw err;
+        }
+      )
+  )
+};
+
+export default solutionModel;
