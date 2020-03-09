3b00e60538c1f7beaed33f32189ac1ae6f38329f
Max Wo <maxwo@protonmail.com>
2020-03-09 22:06:37 +1100

    Solution get model

--- server/models/solution.go
+++ server/models/solution.go
@@ -1,7 +1,29 @@
 package models
 
+import "github.com/maxwowo/examplar/database"
+
 type Solution struct {
 	ID      int    `json:"id"`
 	Content string `json:"content"`
 	ExamID  int    `json:"examId"`
+}
+
+func (s Solution) Get(ID int) (*Solution, error) {
+	db := database.GetDatabase()
+
+	stmt, err := db.Prepare(`
+		SELECT id, content, exam_id
+		FROM solutions
+		WHERE id = $1
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var solution Solution
+
+	err = stmt.QueryRow(ID).Scan(&solution.ID, &solution.Content, &solution.ExamID)
+
+	return &solution, err
 }
