5a78e28b26b25490c0f0743b100ea17102631de2
Max Wo <maxwo@protonmail.com>
2020-03-10 17:33:31 +1100

    Basic exam page layout

--- client/src/pages/ExamPage/ExamPage.tsx
+++ client/src/pages/ExamPage/ExamPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Col, Row } from 'antd';
 
 import PageLayout from '../../components/PageLayout/PageLayout';
+import classes from './ExamPage.module.less';
 
 interface ExamPageMatchingParams {
   examId: string;
@@ -20,7 +22,24 @@ const ExamPage: React.FC<ExamPageProps> = (
 
   return (
     <PageLayout>
-      hi
+      <Row
+        className={classes.examPage}
+        type='flex'
+        justify='space-between'
+      >
+        <Col
+          xs={8}
+          md={6}
+        >
+          ay
+        </Col>
+        <Col
+          xs={16}
+          md={18}
+        >
+          ay!
+        </Col>
+      </Row>
     </PageLayout>
   );
 };
