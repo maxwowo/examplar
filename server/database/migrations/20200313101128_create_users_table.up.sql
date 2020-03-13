4271cf2fbbd248a2817d414725fa1db9a51dbcb9
Max Wo <maxwo@protonmail.com>
2020-03-13 22:38:56 +1100

    Add more columns to users table

--- server/database/migrations/20200313101128_create_users_table.up.sql
+++ server/database/migrations/20200313101128_create_users_table.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id       SERIAL PRIMARY KEY,
-    email    VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL
+    id         SERIAL PRIMARY KEY,
+    email      VARCHAR(100) NOT NULL,
+    password   VARCHAR(100) NOT NULL,
+    created_at DATE         NOT NULL,
+    updated_at DATE         NOT NULL
 );
