1b77c8ec2b3a18d67f5f513a725aedaa6d3cb6fc
Max Wo <maxwo@protonmail.com>
2020-03-13 15:58:26 +1100

    Make Latex component use React components

--- client/src/components/Latex/Latex.tsx
+++ client/src/components/Latex/Latex.tsx
@@ -1,36 +1,19 @@
 import React from 'react';
-
-import classes from './Latex.module.less';
+import 'katex/dist/katex.min.css';
+import LatexNext from 'react-latex-next';
 
 interface LatexProps {
-
+  children?: string;
 }
 
 const Latex: React.FC<LatexProps> = (
   {
     children
   }
-) => {
-  const renderMathJax = () => {
-    (window as any).MathJax.Hub.Queue(['Typeset', (window as any).MathJax.Hub]);
-  };
-
-  React.useEffect(
-    () => {
-      renderMathJax();
-    },
-    []
-  );
-
-  // renderMathJax();
-
-  return (
-    <p
-      className={classes.latex}
-    >
-      {children}
-    </p>
-  );
-};
+) => (
+  <LatexNext>
+    {children}
+  </LatexNext>
+);
 
 export default Latex;
--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import Router from '../pages/Router';
 
-const MATHJAX_OPTIONS = {
-  tex2jax: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']]
-  },
-  showMathMenu: false,
-  showMathMenuMSIE: false
-};
-
-const App: React.FC = () => {
-
-  (window as any).MathJax.Hub.Config(MATHJAX_OPTIONS);
-
-  return (
-    <Router/>
-  );
-};
+const App: React.FC = () => (
+  <Router/>
+);
 
 export default App;
