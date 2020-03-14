83ecf7173242e24e73fee328a291edc1b3ada5bf
Max Wo <maxwo@protonmail.com>
2020-03-14 17:53:09 +1100

    Update user table

--- server/database/migrations/20200313101128_create_users_table.up.sql
+++ server/database/migrations/20200313101128_create_users_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users
 (
     id         SERIAL PRIMARY KEY,
     email      VARCHAR(100) NOT NULL,
+    username   VARCHAR(100) NOT NULL,
     password   VARCHAR(100) NOT NULL,
     created_at DATE         NOT NULL,
     updated_at DATE         NOT NULL
