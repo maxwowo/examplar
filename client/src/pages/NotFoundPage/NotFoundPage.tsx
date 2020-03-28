b1c3e6bc7266a373c91ac1480f1faed4b3db5814
Max Wo <maxwo@protonmail.com>
2020-03-28 16:52:02 +1300

    Improve not found page style

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Typography } from 'antd';
 
 import Space from '../../components/Space/Space';
 import classes from './NotFoundPage.module.less';
@@ -16,25 +15,23 @@ const NotFoundPage: React.FC<NotFoundProps> = () => (
     <section
       className={classes.textSection}
     >
-      <div>
-        <Typography.Title
-          className={classes.title}
+      <h1
+        className={classes.title}
+      >
+        404!
+      </h1>
+      <p
+        className={classes.description}
+      >
+        Page not found,
+        <Space/>
+        <Link
+          to='/'
         >
-          404
-        </Typography.Title>
-        <Typography.Text
-          className={classes.description}
-        >
-          Page not found,
-          <Space/>
-          <Link
-            to='/'
-          >
-            go back home
-          </Link>
-          .
-        </Typography.Text>
-      </div>
+          go back
+        </Link>
+        .
+      </p>
     </section>
   </div>
 );
