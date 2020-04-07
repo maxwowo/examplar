2fe0e4a52612555e95d5f97cfc705e788c800743
Max Wo <maxwo@protonmail.com>
2020-04-07 21:15:16 +1200

    Create users table activated field

--- server/database/migrations/20200313101128_create_users_table.up.sql
+++ server/database/migrations/20200313101128_create_users_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS users
     email      VARCHAR(100) NOT NULL,
     username   VARCHAR(100) NOT NULL,
     password   VARCHAR(100) NOT NULL,
+    activated  BOOLEAN      NOT NULL,
     created_at DATE         NOT NULL,
     updated_at DATE         NOT NULL
 );
--- server/database/migrations/20200327050332_create_registrations_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS registrations
-(
-    id       SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email    VARCHAR(100) NOT NULL
-);
