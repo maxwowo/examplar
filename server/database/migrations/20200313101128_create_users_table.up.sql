6b254c009871f6934cfc81fc36a2100387f892be
Max Wo <maxwo@protonmail.com>
2020-04-08 22:22:09 +1200

    Make password column fixed length

--- server/database/migrations/20200313101128_create_users_table.up.sql
+++ server/database/migrations/20200313101128_create_users_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users
     id        SERIAL PRIMARY KEY,
     username  VARCHAR(100) NOT NULL,
     email     VARCHAR(100) NOT NULL,
-    password  VARCHAR(100) NOT NULL,
+    password  CHAR(44)     NOT NULL,
     activated BOOLEAN      NOT NULL
 );
